5b6403f67e6096a710d88ca36e57595d3bc17fbb
Khurt Ledesma <khurt.ledesma@gmail.com>
2021-01-22 11:12:31 -0600

    added weather API

--- Home/views.py
+++ Home/views.py
@@ -1,19 +1,38 @@
+import requests
 from django.shortcuts import render
 from .models import Climbs
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=319ce877549b8a92d0ca175e58407932'
+    city = 'Austin'
+
+    r = requests.get(weatherUrl.format(city)).json()
+
+    weather = {
+        'city': city,
+        'temp': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'], 
+    }
+    
+    context = {
+        'weather': weather
+        }
+
+    return render(request, 'index.html', context)
 
 def climbs(request):
+
     climbs = Climbs.objects.all()
     count = climbs.count()
     notCompletedClimb = Climbs.objects.filter(completed=False)
     notCompletedClimbCount = notCompletedClimb.count()
     completedClimb = Climbs.objects.filter(completed=True)
     completedClimbCount = completedClimb.count()
-    context = {
+    
+    climbsData = {
         'count': count,
         'climbs': climbs,
         'completedClimb': completedClimb,
@@ -21,6 +40,10 @@ def climbs(request):
         'notCompletedClimb': notCompletedClimb,
         'notCompletedClimbCount': notCompletedClimbCount,
     }
+    context = {
+        'climbsData': climbsData
+    }
+
     return render(request, 'climbs.html', context)
 
 def newClimb(request):
