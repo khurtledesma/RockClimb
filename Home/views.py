43551e95cdbdd5eeaa200c12b104ec7797910e44
Khurt Ledesma <khurt.ledesma@gmail.com>
2021-01-21 21:28:36 -0600

    added completed and uncompleted climbs

--- Home/views.py
+++ Home/views.py
@@ -9,9 +9,17 @@ def index(request):
 def climbs(request):
     climbs = Climbs.objects.all()
     count = climbs.count()
+    notCompletedClimb = Climbs.objects.filter(completed=False)
+    notCompletedClimbCount = notCompletedClimb.count()
+    completedClimb = Climbs.objects.filter(completed=True)
+    completedClimbCount = completedClimb.count()
     context = {
         'count': count,
         'climbs': climbs,
+        'completedClimb': completedClimb,
+        'completedClimbCount': completedClimbCount,
+        'notCompletedClimb': notCompletedClimb,
+        'notCompletedClimbCount': notCompletedClimbCount,
     }
     return render(request, 'climbs.html', context)
 
