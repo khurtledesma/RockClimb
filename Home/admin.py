9b7514c148ea176e8426a2f40baef1158003606d
Khurt Ledesma <khurt.ledesma@gmail.com>
2021-01-21 19:35:05 -0600

    setup basic admin page

--- Home/admin.py
+++ Home/admin.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
-from django.db import models
-from django.utils import timezone
+from .models import Climbs
 
 # Register your models here.
 
-class Climbs(models.Model):
-    BOULDER = 'B'
-    SPORT_CLIMB = "SC"
-    TYPES_OF_CLIMBS = [
-        (BOULDER, 'Boulder'),
-        (SPORT_CLIMB, 'Sport Climb'),
-    ]
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    typeOfClimb = models.CharField(
-        max_length=2,
-        choices=TYPES_OF_CLIMBS,
-    )
+class ClimbsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'typeOfClimb')
     
+admin.site.register(Climbs, ClimbsAdmin)
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Climbs(models.Model):
+    BOULDER = 'B'
+    SPORT_CLIMB = "SC"
+    TYPES_OF_CLIMBS = [
+        (BOULDER, 'Boulder'),
+        (SPORT_CLIMB, 'Sport Climb'),
+    ]
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    typeOfClimb = models.CharField(
+        max_length=2,
+        choices=TYPES_OF_CLIMBS,
+    )
