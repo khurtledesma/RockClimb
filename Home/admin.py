364b476ca78de332cbed98e20497af9801625ee5
Khurt Ledesma <khurt.ledesma@gmail.com>
2021-01-21 19:26:10 -0600

    added base Climbs model

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.db import models
+from django.utils import timezone
 
 # Register your models here.
+
+class Climbs(models.Model):
+    BOULDER = 'B'
+    SPORT_CLIMB = "SC"
+    TYPES_OF_CLIMBS = [
+        (BOULDER, 'Boulder'),
+        (SPORT_CLIMB, 'Sport Climb'),
+    ]
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    typeOfClimb = models.CharField(
+        max_length=2,
+        choices=TYPES_OF_CLIMBS,
+    )
+    
